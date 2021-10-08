7498cb9036f0915413210af0a0f2fbc7bade68aa
Amanda Rozi Kurnia <amandarozikurnia@gmail.com>
2021-10-08 21:18:52 +0700

    Update style.css

--- vertex.js
+++ vertex.js
@@ -10,20 +10,24 @@ function leftPart() {
     var left = [];
     var A, B, C, D, E;
 
-    // atas luas
+    // top outside
     for(var i = 0; i <= 180; i += 1) {
         var j = (i + 270) * Math.PI / 180;
         var k = (i + 1 + 270) * Math.PI / 180;
 
         var vertex1 = [
-            Math.sin(j) * 0.175 - 0.5, // posisi x
-            Math.cos(j) * 0.05 + 0.5, // posisi y
-            0.701, 0.922, 0.541 // warna
+            Math.sin(j) * 0.175 - 0.5, // x
+            Math.cos(j) * 0.05 + 0.5, // y
+            0.16, 0.62, 0.96 // color
         ];
 
         var vertex2 = [
-            -0.5, 0.4, // posisi x dan y
-            0.701, 0.922, 0.541 // warna
+            -0.5, 0.4, //  x dan y
+            0.16, 0.62, 0.96 // color
+        ];
+
+        var vertex3 = [
+
         ]
     }
 }
